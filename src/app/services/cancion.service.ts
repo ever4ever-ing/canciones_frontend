fdd8c919966bd465f7413d9fe0bd5b2e11bc11c7
ever4ever <e.alvarado03@ufromail.cl>
2026-01-14 20:12:20 -0300

    service

--- src/app/services/cancion.service.ts
+++ src/app/services/cancion.service.ts
@@ -7,7 +7,7 @@ import { Cancion } from '../models/cancion.model';
   providedIn: 'root'
 })
 export class CancionService {
-  private apiUrl = 'https://34.176.216.198/canciones';
+  private apiUrl = 'http://34.176.216.198/canciones';
 
   constructor(private http: HttpClient) { }
 
